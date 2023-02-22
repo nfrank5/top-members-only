60102340d644b973829a1dd9698549019bdccc7a
nfrank5 <nfrankenfeld@gmail.com>
2023-02-22 09:38:38 +0800

    Add functionality up to step 6

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,28 @@
+class PostsController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :create]
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    @post.user_id = current_user.id
+    if @post.save
+      redirect_to :root
+    else
+      render :new, status: :unprocessable_entity
+    end
+
+  end
+
+  def index
+    @posts = Post.all
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:body)
+  end
+
+end
